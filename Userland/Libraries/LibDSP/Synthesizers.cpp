42ddf841460d6f15c22ea16a0050ac4c47520cf6
David Isaksson <davidisaksson93@gmail.com>
2021-10-03 14:54:11 +0200

    LibDSP: Minor argument adjustments for Classic synth constructor
    - Insert integer literal separator for consistency's sake
    - Take StringView instead of char* in constructor

--- Userland/Libraries/LibDSP/Synthesizers.cpp
+++ Userland/Libraries/LibDSP/Synthesizers.cpp
@@ -16,10 +16,10 @@ namespace LibDSP::Synthesizers {
 Classic::Classic(NonnullRefPtr<Transport> transport)
     : LibDSP::SynthesizerProcessor(transport)
     , m_waveform("Waveform"sv, Waveform::Saw)
-    , m_attack("Attack"sv, 0, 2000, 5)
+    , m_attack("Attack"sv, 0, 2'000, 5)
     , m_decay("Decay"sv, 0, 20'000, 80)
     , m_sustain("Sustain"sv, 0, 1, 0.725)
-    , m_release("Release", 0, 6'000, 120)
+    , m_release("Release"sv, 0, 6'000, 120)
 {
     m_parameters.append(m_waveform);
     m_parameters.append(m_attack);
